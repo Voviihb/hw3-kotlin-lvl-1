693e2af95ea57bfde068c0348fde44c7c8781b1b
Vladimir Bykov <Voviihb@gmail.com>
2023-12-21 12:32:18 +0300

    logo and background show

--- app/src/main/java/com/voviihb/dz3/MainActivity.kt
+++ app/src/main/java/com/voviihb/dz3/MainActivity.kt
@@ -3,44 +3,56 @@ package com.voviihb.dz3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.voviihb.dz3.ui.theme.DZ3Theme
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DZ3Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .paint(
+                        painterResource(id = R.drawable.background),
+                        contentScale = ContentScale.FillBounds
+                    )
+            ) {
+                Column {
+                    Spacer(modifier = Modifier.height(128.dp))
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp),
+                        verticalAlignment = Alignment.Top,
+                        horizontalArrangement = Arrangement.Center
+                    )
+                    {
+                        Image(
+                            painterResource(id = R.drawable.logo),
+                            contentDescription = getString(R.string.logo),
+                            modifier = Modifier
+                                .size(200.dp)
+
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DZ3Theme {
-        Greeting("Android")
     }
 }
